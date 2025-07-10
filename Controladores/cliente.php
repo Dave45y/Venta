74c079e736376d13c9a43b5482a644d1e26d05b9
Dave Quintero <joquintero2004@gmail.com>
2025-07-09 20:05:18 -0500

    Ver 3 Cliente

--- Controladores/cliente.php
+++ Controladores/cliente.php
@@ -11,15 +11,27 @@ $nombre = isset ($_POST["nombre"]) ? limpiarCadena($_POST["nombre"]) : "";
 
 //Definimos una variable para almacenar el apellido
 
-$condicon = isset ($_POST["apellido"]) ? limpiarCadena($_POST["apellido"]) : "";
+$apellido = isset ($_POST["apellido"]) ? limpiarCadena($_POST["apellido"]) : "";
 
 //Definimos una variable para almacenar el tipo_documento
 
-$condicon = isset ($_POST["tipo_documento"]) ? limpiarCadena($_POST["tipo_documento"]) : "";
+$tipo_documento = isset ($_POST["tipo_documento"]) ? limpiarCadena($_POST["tipo_documento"]) : "";
 
-//Definimos una variable para almacenar el tipo_documento
+//Definimos una variable para almacenar el num_documento
 
-$condicon = isset ($_POST["tipo_documento"]) ? limpiarCadena($_POST["tipo_documento"]) : "";
+$num_documento = isset ($_POST["num_documento"]) ? limpiarCadena($_POST["num_documento"]) : "";
+
+//Definimos una variable para almacenar el dieccion
+
+$dieccion = isset ($_POST["dieccion"]) ? limpiarCadena($_POST["dieccion"]) : "";
+
+//Definimos una variable para almacenar el telefono
+
+$telefono = isset ($_POST["telefono"]) ? limpiarCadena($_POST["telefono"]) : "";
+
+//Definimos una variable para almacenar el email
+
+$email = isset ($_POST["email"]) ? limpiarCadena($_POST["email"]) : "";
 
 
 
@@ -27,28 +39,28 @@ $condicon = isset ($_POST["tipo_documento"]) ? limpiarCadena($_POST["tipo_docume
 //Generamos un switch para determinar la accion a realizar
 switch ($_GET["op"]){
     case 'guardaryeditar':
-        if(empty($idroles)){
-            $rspta = $roles->insertar($nombre,$condicon);
-            echo $rspta ? "Rol registrado con exito" : "No se pudo registrar el rol a la base de datos";
+        if(empty($idcliente)){
+            $rspta = $cliente->insertar($nombre,$apellido,$tipo_documento,$num_documento,$dieccion,$telefono,$email);
+            echo $rspta ? "cliente registrado con exito" : "No se pudo registrar el cliente a la base de datos";
         }
         else{
-            $rspta= $roles->editar($idroles,$nombre,$condicon);
-            echo $rspta ? "Rol editado con exito" :  "No se pudo editar el rol";
+            $rspta= $roles->editar($idcliente,$nombre,$apellido,$tipo_documento,$num_documento,$dieccion,$telefono,$email);
+            echo $rspta ? "Cliente editado con exito" :  "No se pudo editar el cliente";
         }
         break;
         //Si elijo la opcion desactivar ejecuta esta seccion del código
         case 'desactivar':
-            $rspta = $roles->desactivar($idroles);
-            echo $rspta ? "Rol desactivado" : "No se pudo desactivar";
+            $rspta = $cliente->desactivar($idcliente);
+            echo $rspta ? "cliente desactivado" : "No se pudo desactivar";
             break;
 
         case 'activar':
-            $rspta = $roles->activar($idroles);
-            echo $rspta ?"Rol activado" : "No se pudo activar";
+            $rspta = $cliente->activar($idcliente);
+            echo $rspta ?"cliente activado" : "No se pudo activar";
             break;
 
         case 'mostrar' :
-            $rspta= $roles->mostrar($idroles);
+            $rspta= $cliente->mostrar($idcliente);
             //Convertir en resultado en json
             echo json_encode($rspta);
             break;
